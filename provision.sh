082045a5fd94c4d93a34eaf82e8b91f26fda1d53
Archie Brentano <isntall.us@gmail.us>
2015-05-12 11:01:45 -0700

    changing tabs to spaces

--- provision.sh
+++ provision.sh
@@ -22,42 +22,42 @@ export HOME="/home/vagrant"
 
 if [ -f /home/vagrant/drupalci_testbot/PROVISIONED ];
 then
-	echo "--------------------------------------------------------------------"
-	echo
-	echo "######                                      #####  ###"
-	echo "#     # #####  #    # #####    ##   #      #     #  # "
-	echo "#     # #    # #    # #    #  #  #  #      #        # "
-	echo "#     # #    # #    # #    # #    # #      #        # "
-	echo "#     # #####  #    # #####  ###### #      #        # "
-	echo "#     # #   #  #    # #      #    # #      #     #  # "
-	echo "######  #    #  ####  #      #    # ######  #####  ###   TESTBOT"
-	echo ""
-	echo "--------------------------------------------------------------------"
-	echo
-	echo "Hi there, it is your local Testbot!"
-	echo
-	echo "You seem to have this box already installed - which is a good thing!"
-	echo "Documentation can be found in README.md or read on..."
-	echo ""
+  echo "--------------------------------------------------------------------"
+  echo
+  echo "######                                      #####  ###"
+  echo "#     # #####  #    # #####    ##   #      #     #  # "
+  echo "#     # #    # #    # #    #  #  #  #      #        # "
+  echo "#     # #    # #    # #    # #    # #      #        # "
+  echo "#     # #####  #    # #####  ###### #      #        # "
+  echo "#     # #   #  #    # #      #    # #      #     #  # "
+  echo "######  #    #  ####  #      #    # ######  #####  ###   TESTBOT"
+  echo ""
+  echo "--------------------------------------------------------------------"
+  echo
+  echo "Hi there, it is your local Testbot!"
+  echo
+  echo "You seem to have this box already installed - which is a good thing!"
+  echo "Documentation can be found in README.md or read on..."
+  echo ""
 else
-	echo 'cd /home/vagrant/drupalci_testbot' >> /home/vagrant/.bashrc
-	echo 'Defaults        env_keep +="HOME"' >> /etc/sudoers
-	echo "Installing and building the all the things..."
-	echo "on: $(hostname) with user: $(whoami) home: $HOME"
-	swapoff -a
-	dd if=/dev/zero of=/var/swapfile bs=1M count=2048
-	chmod 600 /var/swapfile
-	mkswap /var/swapfile
-	swapon /var/swapfile
-	/bin/echo "/var/swapfile swap swap defaults 0 0" >>/etc/fstab
-	apt-get update && apt-get upgrade -y
-	apt-get install -y git mc ssh gawk grep sudo htop mysql-client php5-cli curl php5-curl \
+  echo 'cd /home/vagrant/drupalci_testbot' >> /home/vagrant/.bashrc
+  echo 'Defaults        env_keep +="HOME"' >> /etc/sudoers
+  echo "Installing and building the all the things..."
+  echo "on: $(hostname) with user: $(whoami) home: $HOME"
+  swapoff -a
+  dd if=/dev/zero of=/var/swapfile bs=1M count=2048
+  chmod 600 /var/swapfile
+  mkswap /var/swapfile
+  swapon /var/swapfile
+  /bin/echo "/var/swapfile swap swap defaults 0 0" >>/etc/fstab
+  apt-get update && apt-get upgrade -y
+  apt-get install -y git mc ssh gawk grep sudo htop mysql-client php5-cli curl php5-curl \
          mysql-client postgresql-client postgresql-client-common
-	apt-get autoclean
+  apt-get autoclean
         echo "Installing docker"
         curl -s get.docker.io | sh 2>&1 | egrep -i -v "Ctrl|docker installed"
         usermod -a -G docker vagrant
-	cd /home/vagrant/drupalci_testbot
+  cd /home/vagrant/drupalci_testbot
   echo "Installing composer"
   curl -sS https://getcomposer.org/installer | php
   echo "Running php composer.phar update"
@@ -71,11 +71,11 @@ else
   mkdir -p /var/lib/drupalci/database/pgsql-9.1
   mkdir -p /var/lib/drupalci/database/pgsql-9.4
   chown -R 102:102 /var/lib/drupalci/database
-	touch PROVISIONED
+  touch PROVISIONED
 fi
 
 chown -fR vagrant:vagrant /home/vagrant
 echo "Box started up, run *vagrant halt* to stop."
 echo
 echo "To access the box and run tests, run:"
-echo "- vagrant ssh"
+echo "- vagrant ssh
