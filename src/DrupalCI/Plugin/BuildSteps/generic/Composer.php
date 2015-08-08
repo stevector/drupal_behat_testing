84dea4ca7b9a8a9010114ae874789c569a8a64ec
amateescu <amateescu@729614.no-reply.drupal.org>
2015-08-08 16:10:55 -0600

    Issue #2315537 by amateescu, timmillwood: adding ComposerCommand

--- src/DrupalCI/Plugin/BuildSteps/generic/Composer.php
+++ src/DrupalCI/Plugin/BuildSteps/generic/Composer.php
@@ -1,30 +1,50 @@
 <?php
+
 /**
  * @file
  * Contains \DrupalCI\Plugin\BuildSteps\generic\Composer
- *
- * Processes "[build_step]: composer:" instructions from within a job definition.
  */
 
 namespace DrupalCI\Plugin\BuildSteps\generic;
-use DrupalCI\Plugin\PluginBase;
+
+use DrupalCI\Plugin\JobTypes\JobInterface;
 
 /**
  * @PluginID("composer")
+ *
+ * Processes "[build_step]: composer:" instructions from within a job
+ * definition.
  */
 class Composer extends ContainerCommand {
 
   /**
    * {@inheritdoc}
+   *
+   * @param string|array $arguments
+   *   Arguments for a composer command. May be a string if one composer command
+   *   is required to run or an array if multiple commands should run.
    */
-  public function run($input, $data) {
-    // @TODO http://stackoverflow.com/a/25208897/308851
-    $cmd = $this->buildComposerCommand($input);
-    parent::run($cmd);
+  public function run(JobInterface $job, $arguments) {
+    // Normalize the arguments to an array format.
+    $arguments_list = (array) $arguments;
+
+    foreach ($arguments_list as $arguments) {
+      $cmd = $this->buildComposerCommand($arguments);
+      parent::run($job, $cmd);
+    }
   }
 
-  protected function buildComposerCommand($input) {
-    // TODO: Code to interpret the input and output an actual valid composer command
+  /**
+   * Returns a full composer command based on the passed-in arguments.
+   *
+   * @param string $arguments
+   *   The arguments for the composer command.
+   *
+   * @return string
+   *   The full composer command string.
+   */
+  protected function buildComposerCommand($arguments) {
+    return "/opt/phpenv/shims/php composer.phar $arguments";
   }
 
 }
