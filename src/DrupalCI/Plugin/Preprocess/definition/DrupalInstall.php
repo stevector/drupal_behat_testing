f6e58977a0bf3559ad05f1524d4edc488cb2a330
Jeremy Thorson <jthorson@sasktel.net>
2015-08-14 18:37:37 -0600

    by jthorson: Remove Composer Install from Drupal Install

--- src/DrupalCI/Plugin/Preprocess/definition/DrupalInstall.php
+++ src/DrupalCI/Plugin/Preprocess/definition/DrupalInstall.php
@@ -45,18 +45,6 @@ class DrupalInstall {
     $this->$function($definition, $core_version, $dci_variables);
   }
 
-  protected function process_composer_install(&$definition, $core_version, $dci_variables) {
-    // Composer installs not supported on D7 core
-    if (substr($core_version, 0, 1) == '7.') {
-      Output::writeLn("<error>The 'Composer' installation method is not compatible with Drupal 7.</error>");
-      // Currently, this will still attempt to run tests and then bail horribly.
-      // TODO: Add more graceful error handling and failure.
-      return;
-    }
-    // Add the 'composer' installation step to the job definition
-    $definition['install']['composer'][] = 'install --working-dir core --prefer-dist';
-  }
-
   protected function process_browser_install(&$definition, $core_version, $dci_variables) {
     // TODO: Implement browser click-through process
 
@@ -68,8 +56,10 @@ class DrupalInstall {
       $definition['install']['command'] = [];
     }
     $definition['install']['command'][] = "cd /var/www/html && /.composer/vendor/bin/drush si -y --db-url=%DCI_DBurl% --clean-url=0 --account-name=admin --account-pass=drupal --account-mail=admin@example.com";
-    $definition['install']['command'][] = "cd /var/www/html && /.composer/vendor/bin/drush vset simpletest_clear_results '0' 2>&1";
-    $definition['install']['command'][] = "cd /var/www/html && /.composer/vendor/bin/drush vset simpletest_verbose '0' 2>&1";
-    $definition['install']['command'][] = "cd /var/www/html && /.composer/vendor/bin/drush en -y simpletest 2>&1";
+    if (substr($core_version, 0, 1) == '7.') {
+      $definition['install']['command'][] = "cd /var/www/html && /.composer/vendor/bin/drush vset simpletest_clear_results '0' 2>&1";
+      $definition['install']['command'][] = "cd /var/www/html && /.composer/vendor/bin/drush vset simpletest_verbose '0' 2>&1";
+      $definition['install']['command'][] = "cd /var/www/html && /.composer/vendor/bin/drush en -y simpletest 2>&1";
+    }
   }
 }
