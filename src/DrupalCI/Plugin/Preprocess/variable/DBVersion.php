43c14666597d94485f10227672ddb24ad8eedf41
isntall <isntall@2899677.no-reply.drupal.org>
2015-09-17 11:53:44 -0700

    Issue #2569749 by isntall: drupalci should work with tagged docker images, simplify the str_replace

--- src/DrupalCI/Plugin/Preprocess/variable/DBVersion.php
+++ src/DrupalCI/Plugin/Preprocess/variable/DBVersion.php
@@ -17,7 +17,7 @@ class DBVersion extends DBUrlBase {
   public function process($db_url, $source_value) {
     $mod_value = explode(':', $source_value, 2)[0];
     $dbtype = explode('-', $mod_value, 2)[0];
-    $host_part = str_replace(':', '-', str_replace('.', '-', $source_value));
+    $host_part = str_replace([':', '.'], '-', $source_value);
     $host = 'drupaltestbot-db-' . $host_part;
     $db_url = $this->changeUrlPart($db_url, 'scheme', $dbtype);
     $db_url = $this->changeUrlPart($db_url, 'host', $host);
