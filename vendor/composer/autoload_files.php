7fad39d7aa7256385572a1ceadf49956ceb121d0
Steve Persch <steve.persch@pantheon.io>
2015-12-29 09:43:11 -0600

    Removing drush from composer

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -10,7 +10,5 @@ return array(
     $vendorDir . '/guzzlehttp/psr7/src/functions_include.php',
     $vendorDir . '/guzzlehttp/promises/src/functions_include.php',
     $vendorDir . '/guzzlehttp/guzzle/src/functions_include.php',
-    $vendorDir . '/symfony/var-dumper/Resources/functions/dump.php',
-    $vendorDir . '/psy/psysh/src/Psy/functions.php',
     $baseDir . '/core/lib/Drupal.php',
 );
